7336214d798da8c98d16326553815c2a41e4344b
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2021-04-21 18:14:41 +0200

    update

---
+++ Task3.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("ji");
+    return 0;
+}
--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -53,5 +53,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/untitled.dir/DependInfo.cmake"
+  "CMakeFiles/Task2.dir/DependInfo.cmake"
+  "CMakeFiles/Task3.dir/DependInfo.cmake"
   )
