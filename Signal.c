d3fd97fc075e0b8e20ab780da6fe6a2478668e19
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2021-05-30 14:41:40 +0200

    updated

--- Signal.c
+++ Signal.c
@@ -2,3 +2,21 @@
 // Created by seka5 on 5/30/2021.
 //
 
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+int(*oldHandler)();
+printf("I can control-C'ed\n");
+sleep(5);
+    oldHandler=signal(SIGINT,SIG_IGN);
+    printf("I am PROTECTED FROM CONTROL-C NOW!!!!!!!!!!\n");
+    sleep(5);
+    signal(SIGINT,oldHandler);
+    printf("i can be control-c again\n");
+    sleep(5);
+    printf("bye\n");
+    return 0;
+
+}
